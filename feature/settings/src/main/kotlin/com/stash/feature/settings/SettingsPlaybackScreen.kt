dd991a97f87bea62907eef53f64d3c0eba12b568
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2026-07-08 04:47:50 -0500

    Add missing import

--- feature/settings/src/main/kotlin/com/stash/feature/settings/SettingsPlaybackScreen.kt
+++ feature/settings/src/main/kotlin/com/stash/feature/settings/SettingsPlaybackScreen.kt
@@ -23,6 +23,7 @@ import com.stash.feature.settings.components.SettingsScaffold
 import com.stash.feature.settings.components.SettingsSectionLabel
 import com.stash.feature.settings.components.SettingsSegmented
 import com.stash.feature.settings.components.SettingsToggleRow
+import kotlin.math.roundToInt
 
 /**
  * The Playback spoke of the hub-and-spoke Settings redesign.
