f6c90066f5dc17fc556390920b85d832fa23c618
Paraliyzed_evo <107338855+ParaliyzedEvo@users.noreply.github.com>
2026-08-22 23:07:23 -0500

    Fix compile error

--- data/ytmusic/src/main/kotlin/com/stash/data/ytmusic/YTMusicApiClient.kt
+++ data/ytmusic/src/main/kotlin/com/stash/data/ytmusic/YTMusicApiClient.kt
@@ -3,10 +3,12 @@ package com.stash.data.ytmusic
 import android.util.Log
 import com.stash.core.model.SyncResult
 import com.stash.data.ytmusic.model.AlbumDetail
+import com.stash.data.ytmusic.model.AlbumSource
 import com.stash.data.ytmusic.model.AlbumSummary
 import com.stash.data.ytmusic.model.ArtistProfile
 import com.stash.data.ytmusic.model.ArtistSummary
 import com.stash.data.ytmusic.model.MusicVideoType
+import com.stash.data.ytmusic.model.PagedAlbums
 import com.stash.data.ytmusic.model.PagedPlaylists
 import com.stash.data.ytmusic.model.PagedTracks
 import com.stash.data.ytmusic.model.SearchAllResults
